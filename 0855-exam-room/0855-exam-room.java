2a2c17fd1af21a381dd45b0435532f65ddbdc9a7
Krishna Sharma <137103145+krishnasharma59@users.noreply.github.com>
2026-07-15 22:37:02 +0530

    Time: 9 ms (100%), Space: 49.1 MB (82.29%) - LeetHub

--- 0855-exam-room/0855-exam-room.java
+++ 0855-exam-room/0855-exam-room.java
@@ -1,43 +1,70 @@
 class ExamRoom {
-    
-    TreeSet<Integer> set;
+
+    class Interval {
+        int l, r, d;
+        
+        Interval(int l, int r) {
+            this.l = l;
+            this.r = r;
+            this.d = dist(l, r);
+        }
+    }
+
     int n;
+    PriorityQueue<Interval> pq;
 
     public ExamRoom(int n) {
-        set = new TreeSet<>();
-        this.n = n-1;
+        this.n = n;
+
+        pq = new PriorityQueue<>((a, b) -> {
+            if(a.d != b.d) return b.d - a.d;
+            return a.l - b.l;
+        });
+
+        pq.offer(new Interval(-1, n));
     }
     
+    private int dist(int l, int r) {
+        if(l == -1) return r;
+        if(r == n) return n - 1 - l;
+        return (r - l) / 2;
+    }
+
     public int seat() {
-        if (set.size() == 0) {
-            set.add(0);
-            return 0;
-        }
         
-        int left = set.first();
-        int diff = left;
-        int max = 0;
-        
-        int mid;
+        Interval curr = pq.poll();
 
-        for (Integer right : set) {
-            if (right == left) continue;
-            mid = left + (right-left)/2;
-            if (diff < Math.min(mid-left, right-mid) && !set.contains(mid)) {
-                diff = Math.min(mid-left, right-mid);
-                max = mid;
-            }
-            left = right;
-        }
+        int pos;
+        if(curr.l == -1) pos = 0;
+        else if(curr.r == n) pos = n - 1;
+        else pos = (curr.l + curr.r) / 2;
 
-        int last = set.last();
-        if (n-last > diff) max = n;
-        
-        set.add(max);
-        return max;
+        pq.offer(new Interval(curr.l, pos));
+        pq.offer(new Interval(pos, curr.r));
+
+        return pos;
     }
     
     public void leave(int p) {
-        set.remove(p);
+        
+        Interval left = null, right = null;
+
+        for(Interval in : pq) {
+            if(in.r == p) left = in;
+            if(in.l == p) right = in;
+            if(left != null && right != null) break;
+        }
+
+        pq.remove(left);
+        pq.remove(right);
+
+        pq.offer(new Interval(left.l, right.r));
     }
 }
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom obj = new ExamRoom(n);
+ * int param_1 = obj.seat();
+ * obj.leave(p);
+ */
